ca0d3acc9fb38c0f147fe274c4d7fc5ff036d59f
mielpops77 <amielzeitoun@gmail.com>
2025-01-01 10:46:56 +0100

    debug bdd distant5

--- Program.cs
+++ Program.cs
@@ -3,26 +3,59 @@ using CoursesRelaxBack.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Activer les paramètres régionaux complets
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers(); // Ajoute le support pour les contrôleurs
+
+// Configure CORS
+var corsPolicyName = "CoursesRelaxCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicyName, policy =>
+    {
+     policy
+            .AllowAnyOrigin() // Autorise toutes les origines
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+
+string connectionString = configuration.GetConnectionString("DefaultConnection");
+
 // Configure DbContext with Azure SQL
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
-// Test the database connection
-using (var scope = app.Services.CreateScope())
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    try
-    {
-        dbContext.Database.OpenConnection(); // Open a connection to test
-        Console.WriteLine("Database connection successful!");
-        dbContext.Database.CloseConnection();
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Database connection failed: {ex.Message}");
-    }
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
+
+app.UseCors(corsPolicyName);
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication(); // Add this line for authentication
+app.UseAuthorization();
+
+app.MapControllers();
 app.Run();
